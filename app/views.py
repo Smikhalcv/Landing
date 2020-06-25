dcea294c1b558208f62a9de7c8ce191740839c06
Smikhalcv <smikhalcv@yandex.ru>
2020-06-25 16:37:34 +0300

    Create landing

---
+++ app/views.py
@@ -0,0 +1,43 @@
+from collections import Counter
+
+from django.shortcuts import render_to_response
+
+# Для отладки механизма ab-тестирования используйте эти счетчики
+# в качестве хранилища количества показов и количества переходов.
+# но помните, что в реальных проектах так не стоит делать
+# так как при перезапуске приложения они обнулятся
+counter_show = Counter()
+counter_click = Counter()
+count_list = []
+
+
+def index(request):
+    # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    return render_to_response('index.html')
+
+
+def landing(request):
+    # Реализуйте дополнительное отображение по шаблону app/landing_alternate.html
+    # в зависимости от GET параметра ab-test-arg
+    # который может принимать значения original и test
+    # Так же реализуйте логику подсчета количества показов
+    ab_test_arg = request.GET.get('lending')
+    if ab_test_arg == 'original':
+        count_list.append('original')
+        return render_to_response('landing.html')
+    elif ab_test_arg == 'test':
+        count_list.append('test')
+        return render_to_response('landing_alternate.html')
+
+
+def stats(request):
+    # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
+    # Чтобы отличить с какой версии лендинга был переход
+    # проверяйте GET параметр marker который может принимать значения test и original
+    # Для вывода результат передайте в следующем формате:
+    for lan in count_list:
+        counter_show[lan] += 1
+    return render_to_response('stats.html', context={
+        'test_conversion': counter_show['test'],
+        'original_conversion': counter_show['original'],
+    })
